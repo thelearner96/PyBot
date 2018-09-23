c15f44e0efb43a57a2b517b14d20c6f0bc84bc7f
thelearner96 <mezzaman2@gmail.com>
2018-09-24 09:47:47 +1000

    Update FunCommands.py

--- modules/FunCommands.py
+++ modules/FunCommands.py
@@ -2,3 +2,15 @@
 import discord
 import os
 import random
+
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
+
+@client.command(pass_context=True)
+async def rolldice(ctx):
+	msg = ctx.message
+	await client.send_message(msg.channel, "%s, you rolled a **%s**!" % (msg.author.mention,random.randint(1,6)))	
+  
+
