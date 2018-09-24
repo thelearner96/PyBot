e6e5b4eccd64d461ead3cdb208efedf764fc8881
thelearner96 <mezzaman2@gmail.com>
2018-09-24 15:52:00 +1000

    Update FunCommands.py

--- modules/FunCommands.py
+++ modules/FunCommands.py
@@ -8,9 +8,15 @@ from discord.ext import commands
 import asyncio
 import time
 
-@client.command(pass_context=True)
-async def hi(ctx):
-	msg = ctx.message
-	await client.send_message(msg.channel, "hello")	
+Class Funcommands:
+	def __init__:
+		Pass
+	
+	@commands.command(name="hi",pass_context=True)
+	async def hi(ctx):
+		msg = ctx.message
+		await client.send_message(msg.channel, "hello")	
+
+
   
 
